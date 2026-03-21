546a755e0526ec41bf78225a72abd3ea4d47acb3
Veyrost <142344499+Khayom1@users.noreply.github.com>
2026-03-21 20:40:51 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+
+# Токени аз @BotFather гирифтаатонро дар байни нохунакҳо гузоред
+API_TOKEN = 'ИН_ҶО_ТОКЕН_ГУЗОРЕД'
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def cmd_start(message: types.Message):
+    await message.answer(f"Салом {message.from_user.first_name}! Ман боти шахсии Delta Force TJ ҳастам. Фармонро интизорам!")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
+
